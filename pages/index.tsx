ba9fc8fece852f7d619291e7c2a893cd786e6623
Globallager <72797635+Globallager@users.noreply.github.com>
2022-07-11 15:48:50 +0800

    Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -170,133 +170,124 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
-      <h1>Aztec Interactive</h1>
-      {hasMetamask ? (
-        isConnected ? (
-          <div className="new-line">
-            <h2>Account Info</h2>
-            {`Ethereum Address: ${ethAccount}
-            zkETH Balance: ${zkEthBalance}
-            Privacy Public Key: ${accountPublicKey}
-            Privacy Private Key: ${JSON.stringify(accountPrivateKey)}
-            Spending Public Key: ${spendingSigner?.publicKey}
-            Spending Key: ${JSON.stringify(spendingSigner?.privateKey)}
-            
-            `}
+    <div className="w-screen h-screen flex justify-center">
+      <div className="max-w-prose">
+        {hasMetamask ? (
+          isConnected ? (
+            <div>
+              <h1>Account Info</h1>
+              <table className="break-all">
+                <thead>
+                  <tr>
+                    <th className="w-44">Property</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Ethereum Address</td>
+                    <td>{`${ethAccount}`}</td>
+                  </tr>
+                  <tr>
+                    <td>zkETH Balance</td>
+                    <td>{`${zkEthBalance}`}</td>
+                  </tr>
+                  <tr>
+                    <td>[Public] Privacy Key</td>
+                    <td>{`${accountPublicKey}`}</td>
+                  </tr>
+                  <tr>
+                    <td>[Private] Privacy Key</td>
+                    <td>{`${JSON.stringify(accountPrivateKey)}`}</td>
+                  </tr>
+                  <tr>
+                    <td>[Public] Spending Key</td>
+                    <td>{`${spendingSigner?.publicKey}`}</td>
+                  </tr>
+                  <tr>
+                    <td>[Private] Spending Key</td>
+                    <td>{`${JSON.stringify(spendingSigner?.privateKey)}`}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            // Step 1 - Connect Metamask
+            <div>
+              <h1>Connect Metamask</h1>
+              <p>Welcome to Aztec Interactive. This tutorial will walk you through the process of using Aztec. Before we begin, you would need a Metamask wallet funded with &gt;0.02 ETH on the Goerli Testnet.</p>
+              <p>Once that is ready, connect your wallet with the button below.</p>
+              <button className="bg-gradient-to-r from-pink-500 to-yellow-500" onClick={() => connect()}>Connect</button>
+            </div>
+          )
+        ) : (
+          "Please install metamask"
+        )}
+        {connecting ? "Loading..." : ""}
+        {sdk ? (
+          <div>
+            <button className="bg-gradient-to-r from-gray-400 to-gray-600" onClick={() => console.log("sdk", sdk)}>[Debug] Log SDK</button>
+            {accountPrivateKey && !account0 ? (
+              // Step 3 - Explain Privacy Keys + Update Account Balance
+              <div>
+                <h1>Update Account Balance</h1>
+                <p>Your privacy key pair is now generated. This key pair is used to decrypt your Aztec value notes, enabling you to retrieve your account balance on Aztec while maintaining such information encrypted from public knowledge (hence privacy).</p>
+                <p>Now that your privacy key pair is ready, you can check your ETH balance available on Aztec with the button below. You should see "zkETH Balance" in your account info above updated once the process completes. If this is your first time interacting with Aztec and no previous deposits were made, you should see a 0 balance.</p>
+              </div>
+            ) : (
+              ""
+            )}
+            {accountPrivateKey ? (
+              <button onClick={() => initUsersAndPrintBalances()}>
+                Update zkETH Balance
+              </button>
+            ) : (
+              // Step 2 - Generate Privacy Key Pair
+              <div>
+                <h1>Generate Privacy Key Pair</h1>
+                <p>With your wallet connected, you can now generate the privacy key pair corresponding to your wallet for account registration / login on Aztec.</p>
+                <p>An Ethereum public / private key cannot derive an account address / sign transactions directly on Aztec, as Aztec uses a different elliptic curve for cryptographic signature (Grumpkin) than Ethereum (ECDSA) for more efficient SNARK operations.</p>
+                <p>To ensure users' access to their Aztec accounts as long as they possess access to their Ethereum accounts, Aztec account keys are derived from messages signed by the user using his/her Ethereum account. Different messages are used to generate different key pairs (privacy / spending). You can generate the privacy key pair using the button below. You should see "Privacy Public Key" and "Privacy Private Key" in your account info above updated once the process completes.</p>
+                <button onClick={() => login()}>Generate Privacy Keys</button>
+              </div>
+            )}
+            {spendingSigner && account0 ? (
+              <button onClick={() => depositEth()}>Deposit 0.01 ETH</button>
+            ) : (
+              ""
+            )}
+            {spendingSigner && !userExists ? (
+              // Step 5 - Register Account
+              <div>
+                <h1>Register Account</h1>
+                <p>At this point, account information from previous steps were all generated locally off-chain. To fully enjoy the features Aztec offers, you would have to register an account on Aztec using such information.</p>
+                <p>The registration ties a human-readable account alias (≤20 alphanumeric, lowercase characters) to your privacy keys and spending public key, helpful for easing the value transfer UX on Aztec. In production, the user should be offered the choice of a preferred alias, but for testing purpose it is hardcoded as "test232" here.</p>
+                <p>The registration also requires ETH value attached to the transaction. The value consists of a minimum deposit of 0.01 ETH (later spendable on Aztec) plus the Aztec network fee for registering the account. In production, the network fee should be set as NEXT_ROLLUP, which batches the user's registration into the next rollup batch at the benefit of lower fees. Yet for testing purpose INSTANT fees is used here instead.</p>
+                <p>Now that your keys are ready, you can register your Aztec account with the button below.</p>
+                <button onClick={() => registerNewAccount()}>Register Account</button>
+              </div>
+            ) : (
+              ""
+            )}
+            {!spendingSigner && account0 ? (
+              // Step 4 - Generate Spending Key Pair
+              <div>
+                <h1>Generate Spending Key Pair</h1>
+                <p>Similar to privacy keys, a separate set of spending key pair can be generated through signing a different message. This key pair is used to spend your Aztec value notes.</p>
+                <p>The separation of the key required to decrypt notes (privacy key) and the key required to spend notes (spending key) theoretically enables users to take advantage of account abstraction. A user for example may generate multiple spending keys to be used on multiple devices respectively, avoiding the risk of sharing the underlying Ethereum account on different devices.</p>
+                <p>Aztec value notes are also only spendable by the specific note receiver's spending key marked when the note was created. Using multiple spending keys across environments can also be beneficial to limiting risks against fund losses due to environmental compromisation.</p>
+                <p>At this moment however generating one set of spending keys is encouraged for simple UX.</p>
+                <p>You can generate a spending key pair using the button below. You should see "Spending Public Key" and "Spending Private Key" in your account info above updated once the process completes.</p>
+                <button onClick={() => getSpendingKey()}>Generate Spending Keys</button>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         ) : (
-          // Step 1 - Connect Metamask
-          <div>
-            <h2>Connect Metamask</h2>
-            <div className="new-line">
-              {`Welcome to Aztec Interactive. This tutorial will walk you through the process of using Aztec.
-
-              Before we begin, you would need a Metamask wallet funded with >0.02 ETH on the Goerli Testnet.
-              
-              Once that is ready, connect your wallet with the button below.
-              
-              `}
-            </div>
-            <button onClick={() => connect()}>Connect</button>
-          </div>
-        )
-      ) : (
-        "Please install metamask"
-      )}
-      {connecting ? "Loading..." : ""}
-      {sdk ? (
-        <div>
-          <button onClick={() => console.log("sdk", sdk)}>[Debug] Log SDK</button>
-          {accountPrivateKey && !account0 ? (
-            // Step 3 - Explain Privacy Keys + Update Account Balance
-            <div>
-              <h2>Update Account Balance</h2>
-              <div className="new-line">
-                {`Your privacy key pair is now generated. This key pair is used to decrypt your Aztec value notes, enabling you to retrieve your account balance on Aztec while maintaining such information encrypted from public knowledge (hence privacy).
-                  
-                  Now that your privacy key pair is ready, you can check your ETH balance available on Aztec with the button below. You should see "zkETH Balance" in your account info above updated once the process completes. If this is your first time interacting with Aztec and no previous deposits were made, you should see a 0 balance.
-                  
-                  `}
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-          {accountPrivateKey ? (
-            <button onClick={() => initUsersAndPrintBalances()}>
-              Update zkETH Balance
-            </button>
-          ) : (
-            // Step 2 - Generate Privacy Key Pair
-            <div>
-              <h2>Generate Privacy Key Pair</h2>
-              <div className="new-line">
-                {`With your wallet connected, you can now generate the privacy key pair corresponding to your wallet for account registration / login on Aztec.
-
-                  An Ethereum public / private key cannot derive an account address / sign transactions directly on Aztec, as Aztec uses a different elliptic curve for cryptographic signature (Grumpkin) than Ethereum (ECDSA) for more efficient SNARK operations. 
-
-                  To ensure users' access to their Aztec accounts as long as they possess access to their Ethereum accounts, Aztec account keys are derived from messages signed by the user using his/her Ethereum account. Different messages are used to generate different key pairs (privacy / spending). You can generate the privacy key pair using the button below. You should see "Privacy Public Key" and "Privacy Private Key" in your account info above updated once the process completes.
-
-                `}
-              </div>
-              <button onClick={() => login()}>Generate Privacy Keys</button>
-            </div>
-          )}
-          {spendingSigner && account0 ? (
-            <button onClick={() => depositEth()}>Deposit 0.01 ETH</button>
-          ) : (
-            ""
-          )}
-          {spendingSigner && !userExists ? (
-            // Step 5 - Register Account
-            <div>
-              <h2>Register Account</h2>
-              <div className="new-line">
-                {`At this point, account information from previous steps were all generated locally off-chain. To fully enjoy the features Aztec offers, you would have to register an account on Aztec using such information.
-
-                The registration ties a human-readable account alias (≤20 alphanumeric, lowercase characters) to your privacy keys and spending public key, helpful for easing the value transfer UX on Aztec. Practically the user should be offered the choice of his/her preferred alias, but for testing purpose it is hardcoded as "test232" here.
-
-                The registration also requires ETH value attached to the transaction. The value consists of a minimum deposit of 0.01 ETH (later spendable on Aztec) plus the Aztec network fee for registering the account. Practically the network fee should be set as NEXT_ROLLUP, which batches the user's registration into the next rollup batch at the benefit of lower fees. Yet for testing purpose INSTANT fees is used here instead.
-
-                Now that your keys are ready, you can register your Aztec account with the button below.
-                `}
-              </div>
-              <button onClick={() => registerNewAccount()}>
-                Register Account
-              </button>
-            </div>
-          ) : (
-            ""
-          )}
-          {!spendingSigner && account0 ? (
-            // Step 4 - Generate Spending Key Pair
-            <div>
-              <h2>Generate Spending Key Pair</h2>
-              <div className="new-line">
-                {`Similar to privacy keys, a separate set of spending key pair can be generated through signing a different message. This key pair is used to spend your Aztec value notes.
-                
-                The separation of the key required to decrypt notes (privacy key) and the key required to spend notes (spending key) theoretically enables users to take advantage of account abstraction. A user for example may generate multiple spending keys to be used on multiple devices respectively, avoiding the risk of sharing the underlying Ethereum account on different devices.
-
-                Aztec value notes are also only spendable by the specific note receiver's spending key marked when the note was created. Using multiple spending keys across environments can also be beneficial to limiting risks against fund losses due to environmental compromisation.
-                
-                At this moment however generating one set of spending keys is encouraged for simple UX.
-
-                You can generate a spending key pair using the button below. You should see "Spending Public Key" and "Spending Private Key" in your account info above updated once the process completes.
-
-                `}
-              </div>
-              <button onClick={() => getSpendingKey()}>
-                Generate Spending Keys
-              </button>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      ) : (
-        ""
-      )}
+          ""
+        )}
+      </div>
     </div>
   );
 };
